43f86dcc35162b2a9fa1ca81cca6dda0db26af6a
SeonJiWon <tommys915@gmail.com>
2020-11-14 21:45:13 +0900

    Second Commit

--- app/src/main/java/org/techtown/accountbook/Outcome_menu.java
+++ app/src/main/java/org/techtown/accountbook/Outcome_menu.java
@@ -1,14 +1,14 @@
 package org.techtown.accountbook;
 
-import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
 
 public class Outcome_menu extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_income_menu);
+        setContentView(R.layout.activity_outcome_menu);
     }
 }
